4eb20ca67fbd83a2c021e63cfbdcf217741a7835
Guilherme Gaspar <guilhermevgaspar07@gmail.com>
2025-11-03 22:20:46 -0300

    Implementation of initial Roll Call layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,12 +38,17 @@ android {
 }
 
 dependencies {
-
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/data/Student.kt
@@ -0,0 +1,7 @@
+package data
+
+data class Student(
+    val name: String,
+    val registration: String,
+    val present: Boolean,
+)
